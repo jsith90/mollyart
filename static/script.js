5e249d1862455e8e013d1028500573c7531c4912
jsith90 <j.sinclairthomson@gmail.com>
2024-08-27 14:58:33 +0200

    style trolley

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 const shopItem = document.querySelectorAll('.shop-item');
+const checkout = document.querySelectorAll('.checking-out')
 
 
 shopItem.forEach((item, index) => {
@@ -36,8 +37,30 @@ shopItem.forEach((item, index) => {
         	badge.style.border = "black";
         });
         availableBadge.forEach(badge => {
-        	badge.style.background = "green";
-        	badge.style.border = "green";
+        	badge.style.background = "#3f9c44";
+        	badge.style.border = "#3f9c44";
         });
 	};
+});
+
+checkout.forEach((item, index) => {
+    item.onmouseover = function() {
+        checkout[index].style.cursor = "pointer"; 
+        const shopBadge = item.querySelectorAll('.view-product-available');
+        shopBadge.forEach(badge => {
+            badge.style.background = "#4eb2ff";
+            badge.style.border = "#4eb2ff";
+        });
+    };
+});
+
+checkout.forEach((item, index) => {
+    item.onmouseout = function() {
+        checkout[index].style.cursor = "pointer"; 
+        const shopBadge = item.querySelectorAll('.view-product-available');
+        shopBadge.forEach(badge => {
+            badge.style.background = "#3f9c44";
+            badge.style.border = "#3f9c44";
+        });
+    };
 });
