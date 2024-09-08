4b4720383680c72908e62a29e592863392066454
jsith90 <j.sinclairthomson@gmail.com>
2024-09-08 12:10:00 +0200

    css update

--- payment/forms.py
+++ payment/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from .models import ShippingAddress
 
 class ShippingForm(forms.ModelForm):
-	shipping_full_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Full Name'}), required=True)
-	shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}), required=True)
-	shipping_address1 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address 1'}), required=True)
+	shipping_full_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'*Full Name'}), required=True)
+	shipping_email = forms.EmailField(label="", widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'*Email'}), required=True)
+	shipping_address1 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'*Address 1'}), required=True)
 	shipping_address2 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address 2'}), required=False)
-	shipping_city = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'City'}), required=True)
-	shipping_region = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Region'}), required=True)
-	shipping_postcode = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Postcode'}), required=True)
-	shipping_country = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Country'}), required=True)
+	shipping_city = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'*City'}), required=True)
+	shipping_region = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'*Region'}), required=True)
+	shipping_postcode = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'*Postcode'}), required=True)
+	shipping_country = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'*Country'}), required=True)
 
 	class Meta:
 		model = ShippingAddress
