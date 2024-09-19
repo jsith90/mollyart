a700fda43a899d3dd26c1444369f7ac5dbcdb777
jsith90 <j.sinclairthomson@gmail.com>
2024-09-19 12:55:48 +0200

    fix js break

--- static/script.js
+++ static/script.js
@@ -140,38 +140,43 @@ function navScroll() {
         hatmanDiv.style.display = "flex";
     });
 
-    // set index and transition delay
-    let index = 0;
-    let transitionDelay = 10000;
+    if (reviews) {
 
-    // get div containing the slides
-    let slideContainer = document.querySelector(".slideshow");
-    // get the slides
-    let slides = slideContainer.querySelectorAll(".slide");
+        // set index and transition delay
+        let index = 0;
+        let transitionDelay = 10000;
 
-    // set transition delay for slides
-    for (let slide of slides) {
-      slide.style.transition = `opacity 2s ease-in-out`;
-    }
+        // get div containing the slides
+        let slideContainer = document.querySelector(".slideshow");
+        // get the slides
+        let slides = slideContainer.querySelectorAll(".slide");
 
-    // show the first slide
-    showSlide(index);
-
-    // show a specific slide
-    function showSlide(slideNumber) {
-        slides.forEach((slide, i) => {
-        slide.style.opacity = i == slideNumber ? "1" : "0";
-        });
-        // next index
-        index++;
-        // go back to 0 if at the end of slides
-        if (index >= slides.length) {
-            index = 0;
+        // set transition delay for slides
+        for (let slide of slides) {
+          slide.style.transition = `opacity 2s ease-in-out`;
         }
-    }
 
-    // transition to next slide every x seconds
-    setInterval (() => showSlide(index), transitionDelay);
+        // show the first slide
+        showSlide(index);
+
+        // show a specific slide
+        function showSlide(slideNumber) {
+            slides.forEach((slide, i) => {
+            slide.style.opacity = i == slideNumber ? "1" : "0";
+            });
+            // next index
+            index++;
+            // go back to 0 if at the end of slides
+            if (index >= slides.length) {
+                index = 0;
+            }
+        }
+
+        // transition to next slide every x seconds
+        setInterval (() => showSlide(index), transitionDelay);
+    } else {
+        console.log('no reviews here.');
+    }
 }
 
 function navBar() {
